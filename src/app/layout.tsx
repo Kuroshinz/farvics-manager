2a71513c97a18f4f5f8ee637c09e1ac21fc8cbb3
Nz <158820515+Kuroshinz@users.noreply.github.com>
2026-08-07 16:46:38 +0700

    fix(ui): import Toaster in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+import { Toaster } from 'sonner';\nexport const dynamic = 'force-dynamic';
 ﻿import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
